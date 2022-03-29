c09e91a199debef43153bb4d12e371b7dab2fb20
Venkataramana48 <yegireddi.venkataramana@gmail.com>
2022-03-29 18:05:40 +0530

    updated to the logintest

---
+++ src/test/java/com/qa/hs/logInPage/LoginTest.java
@@ -0,0 +1,59 @@
+package logInPage;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import excelUtility.ReadExcelSheet;
+import utility.Constant;
+
+public class LoginTest {
+	
+	public static WebDriver driver;
+	
+	public void loginSetUp() throws IOException {
+		
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Jars\\Selenium Projects\\LocalBadabing Project with Selenium"
+				+ "\\Badabing Project\\DataDrivenFrameworkUsingPOI\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		
+		driver.manage().window().maximize();
+		
+		
+		driver.get(Constant.URL);
+		
+		driver.manage().timeouts().implicitlyWait(Constant.implicitlyWait, TimeUnit.SECONDS);
+		
+		ReadExcelSheet rs = new ReadExcelSheet();
+		
+		ArrayList userName = rs.readExcelData(1);
+		ArrayList passWord = rs.readExcelData(2);
+		
+		for(int i = 0; i<userName.size(); i++) {
+			
+		WebElement username = driver.findElement(By.id("username"));
+		String str1 = (String) userName.get(i);
+		username.sendKeys(str1);
+		
+		WebElement password = driver.findElement(By.id("password"));
+		String str2 = (String) passWord.get(i);
+		password.sendKeys(str2);
+		
+		WebElement signIn = driver.findElement(By.xpath("//button[contains(text(),'Sign in')]"));
+		signIn.click();
+		
+		
+		}
+		
+		
+		
+		
+	}
+
+}
