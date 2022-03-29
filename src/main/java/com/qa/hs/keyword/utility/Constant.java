574976974656a3271bc4891cffcc4a6b19f16ca6
Venkataramana48 <yegireddi.venkataramana@gmail.com>
2022-03-29 17:56:00 +0530

    updated with utility folder

---
+++ src/main/java/com/qa/hs/keyword/utility/Constant.java
@@ -0,0 +1,12 @@
+package utility;
+
+public class Constant {
+	
+	public static final String URL = "https://www.linkedin.com/uas/login?";
+	
+	public static final String filePath = "D:\\Selenium Jars\\Selenium Projects\\LocalBadabing Project with Selenium"
+			+ "\\Badabing Project\\DataDrivenFrameworkUsingPOI\\src\\testData\\DDFSheet.xlsx";
+	
+	public static int implicitlyWait = 30;
+
+}
